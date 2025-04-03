5b2281608284ffc93b48f8582f75c37f18d479d5
Carla Sousa <sousa.carla09@gmail.com>
2025-04-02 21:43:11 -0300

    feat: first page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeSearchComponent } from './pages/home-search/home-search.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: HomeSearchComponent
+  }
+];
+
+// página inicial onde busca o usuário
+// página que lista todos os repositórios daquele usuário
+// página de detalhes do repositorio 
---
+++ src/app/pages/home-search/home-search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-search',
+  imports: [],
+  templateUrl: './home-search.component.html',
+  styleUrl: './home-search.component.scss'
+})
+export class HomeSearchComponent {
+
+}
