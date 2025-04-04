9dfb812e1725ea2f83f2e5928d76ca6df8c19a69
Carla Sousa <sousa.carla09@gmail.com>
2025-04-04 10:24:30 -0300

    feat: create first page

--- src/app/pages/home-search/home-search.component.ts
+++ src/app/pages/home-search/home-search.component.ts
@@ -1,11 +1,19 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-home-search',
-  imports: [],
+  imports: [ReactiveFormsModule, NgOptimizedImage],
   templateUrl: './home-search.component.html',
   styleUrl: './home-search.component.scss'
 })
 export class HomeSearchComponent {
+  githubUser = new FormGroup({
+    username: new FormControl('', Validators.required),
+  });
 
+  handleSubmit() {
+    console.log(this.githubUser.value.username);
+  }
 }
