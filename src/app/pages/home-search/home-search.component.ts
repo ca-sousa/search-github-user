b24a42cc51fc7d3ed65c548398b1fad5c40c99eb
Carla Sousa <sousa.carla09@gmail.com>
2025-04-05 20:04:07 -0300

    feat: make request after submit

--- src/app/pages/home-search/home-search.component.ts
+++ src/app/pages/home-search/home-search.component.ts
@@ -1,20 +1,41 @@
 import { NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import { UserInformationComponent } from '../../shared/components/user-information/user-information.component';
+import { GithubService } from '../../services/github.service';
+import { GithubUser } from '../../interfaces/github';
 
 @Component({
   selector: 'app-home-search',
   imports: [ReactiveFormsModule, NgOptimizedImage, UserInformationComponent],
+  providers: [GithubService],
   templateUrl: './home-search.component.html',
   styleUrl: './home-search.component.scss'
 })
 export class HomeSearchComponent {
-  githubUser = new FormGroup({
-    username: new FormControl('', Validators.required),
-  });
+  githubUser!: FormGroup;
+  loading = signal(false);
+  userInformation: GithubUser[] = [];
+
+  constructor(private service: GithubService) {
+    this.githubUser = new FormGroup({
+      username: new FormControl('', Validators.required),
+    });
+  }
 
   handleSubmit() {
-    console.log(this.githubUser.value.username);
+    this.loading.set(true);
+    if (this.githubUser.valid) {
+      this.service.getGithubUser(this.githubUser.value.username).subscribe({
+        next: (res) => {
+          if (res) {
+            this.userInformation = res;
+            console.log(res)
+          }
+          this.loading.set(false);
+          this.githubUser.reset();
+        }
+      });
+    }
   }
 }
